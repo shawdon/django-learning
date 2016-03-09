630122741f92d1272204408c7bdc2d40e5c97dc3
shawdon <njuzhangxiao@gmail.com>
2016-03-09 09:55:11 +0000

    design url for category

--- rango/admin.py
+++ rango/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from rango.models import Category, Page
 
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 class PageAdmin(admin.ModelAdmin):
 	"""docstring for PageAdmin"""
 	list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
