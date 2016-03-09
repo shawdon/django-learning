f5d0c5de537455a574e67f9c37195f003aeb9a6b
shawdon <njuzhangxiao@gmail.com>
2016-03-09 06:10:58 +0000

    revise model and the page interface

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+	"""docstring for PageAdmin"""
+	list_display = ('title', 'category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
